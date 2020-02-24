b87b6f8683e66f39b2d1a4e11c55a312620cc83e
vladsmetansky <vladsmetansky@gmail.com>
2020-02-24 11:21:36 +0200

    lab1 done

--- fx_graphics/src/sample/components/Axe.java
+++ fx_graphics/src/sample/components/Axe.java
@@ -3,7 +3,6 @@ package sample.components;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
